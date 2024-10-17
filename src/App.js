8d640eb310e85a4bd8f5de2b09ba7f1e884c7c89
Vũ Văn Cao <vuvancao@MacBook-Air-cua-Vu.local>
2024-10-18 00:22:37 +0900

    compoent

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
 
+import MyComponent from './components/MyComponent';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Helloworld with reactjs
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Heloworld with reactjs
+      <MyComponent></MyComponent>
     </div>
+
+
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Heloworld with reactjs
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
---
+++ src/components/MyComponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class MyComponent extends React.Component {
+    //tap hop cac khoi html(jsx) chuyen thanh giao dien(UI)
+    render() {
+        return (
+            <div> my first component</div>
+        )
+    }
+}
+export default MyComponent;
